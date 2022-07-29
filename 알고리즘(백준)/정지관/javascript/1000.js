30ceaa51f9a178c4941b89406b10a2c3274cfa64
jikwan <jikwan0327@gmail.com>
2022-07-29 23:56:43 +0900

    0729 commit
    baekjoon

---
+++ 알고리즘(백준)/정지관/javascript/1000.js
@@ -0,0 +1,6 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+
+console.log(a + b);
---
+++ 알고리즘(백준)/정지관/javascript/10869.js
@@ -0,0 +1,10 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
