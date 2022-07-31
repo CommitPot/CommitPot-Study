dd708aca5b5964db5618a60066bcc50a18991ca5
Kim Yeon Woo <102429539+yeon0821@users.noreply.github.com>
2022-07-31 23:27:31 +0900

    13277번.kt

---
+++ 알고리즘(백준)/김연우/Kotlin/13277.kt
@@ -0,0 +1,12 @@
+import java.math.BigInteger
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    val sc: Scanner = Scanner(System.`in`)
+    var a = BigInteger(sc.next())
+    var b = BigInteger(sc.next())
+
+    println(a*b)
+
+
+}
