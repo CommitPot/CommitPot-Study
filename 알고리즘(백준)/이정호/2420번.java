a26f89b24b070affd6d446ff5eb41f5a7cf2d5f5
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-02 19:50:34 +0900

    ⚡ :: 2420번

---
+++ 알고리즘(백준)/이정호/2420번.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextInt(), b = sc.nextInt();
+        System.out.println(Math.abs(a - b));
+    }
+}
