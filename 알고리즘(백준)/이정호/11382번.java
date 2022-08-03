d3d4017c179ae9aa3c87a519edaaaba6152a40c7
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-08-03 12:49:28 +0900

    ⚡ :: 11382번

---
+++ 알고리즘(백준)/이정호/11382번.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a, b, c;
+        a = sc.nextLong();
+        b = sc.nextLong();
+        c = sc.nextLong();
+        System.out.println(a+b+c);
+    }
+}
