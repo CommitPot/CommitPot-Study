c07924dfb4554fc0a2d18d78774215cd2c434ad8
Kim Yeon Woo <102429539+yeon0821@users.noreply.github.com>
2022-08-04 11:13:08 +0900

    10170번.kt

---
+++ 알고리즘(백준)/김연우/Kotlin/101070.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+fun main(args: Array<String>){
+    val sc = Scanner(System.`in`)
+
+        println("NFC West       W   L  T")
+        println("-----------------------")
+        println("Seattle        13  3  0")
+        println("San Francisco  12  4  0")
+        println("Arizona        10  6  0")
+        println("St. Louis      7   9  0")
+        println("")
+        println("NFC North      W   L  T")
+        println("-----------------------")
+        println("Green Bay      8   7  1")
+        println("Chicago        8   8  0")
+        println("Detroit        7   9  0")
+        println("Minnesota      5  10  1")
+}
