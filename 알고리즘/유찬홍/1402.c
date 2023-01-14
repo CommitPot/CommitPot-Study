99b53cb0d5815b70b1b0da4d127fdbfd5d101769
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-14 20:44:33 +0900

    백준 1402번

---
+++ 알고리즘/유찬홍/1402.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n, a, b;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &a, &b);
+        printf("yes\n");
+    }
+    return 0;
+}
