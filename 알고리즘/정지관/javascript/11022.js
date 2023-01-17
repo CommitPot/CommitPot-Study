dd7036c0b9e447c7092bd49a66707ac6b7c8f9ac
jikwan <jikwan0327@gmail.com>
2023-01-17 16:35:29 +0900

    백준 11022번

---
+++ 알고리즘/정지관/javascript/11022.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const size = parseInt(input[0]);
+
+for (let i = 1; i <= size; i++) {
+  let result = 0;
+  let scan = input[i].split(" ");
+  result = Number(scan[0]) + Number(scan[1]);
+  console.log(`Case #${i}: ${scan[0]} + ${scan[1]} = ${result}`);
+}
