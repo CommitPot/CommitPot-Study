2da9761fe7ca5d810952213fcadd031711769faa
jikwan <jikwan0327@gmail.com>
2023-01-17 16:45:53 +0900

    백준 2438번

---
+++ 알고리즘/정지관/javascript/2438.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString();
+
+const size = parseInt(input);
+
+for (let i = 1; i <= size; i++) {
+  let answer = "";
+  for (let j = 0; j < i; j++) {
+    answer += "*";
+  }
+  console.log(answer);
+}
