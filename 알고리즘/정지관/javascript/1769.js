7002dc97d2caf09d00635a839c0c22021624c7c1
jikwan <jikwan0327@gmail.com>
2023-01-18 11:20:34 +0900

    백준 1769번

--- 알고리즘/정지관/javascript/1769.js
+++ 알고리즘/정지관/javascript/1769.js
@@ -1,4 +1,19 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString();
 
-console.log(input);
+let result = input;
+let total = 0;
+let progress = 0;
+
+while (result.length !== 1) {
+  if (Number(result) < 10) break;
+  for (let i = 0; i < result.length; i++) {
+    total += Number(result[i]);
+  }
+  progress++;
+  result = total.toString();
+  total = 0;
+}
+
+console.log(progress);
+console.log(result % 3 === 0 ? "YES" : "NO");
