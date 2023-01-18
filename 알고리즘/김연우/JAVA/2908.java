f7cec8fe5173fa471466c3527729a08ddd2e62f9
Yeonu <102429539+yeon0821@users.noreply.github.com>
2023-01-18 23:44:36 +0900

    2908.java

---
+++ 알고리즘/김연우/JAVA/2908.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+        
+		int A = in.nextInt();
+		int B = in.nextInt();
+        
+        in.close();
+        
+		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+		
+		System.out.print(A > B ? A : B);
+	
+	}
+}
