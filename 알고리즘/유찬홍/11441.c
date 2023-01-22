0d51278fc4e64f1ac38401a750dd6368463d5bf0
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-22 11:00:36 +0900

    백준 11441번

---
+++ 알고리즘/유찬홍/11441.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, m, a, b, arr[100001] = {0};
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &arr[i]);
+        arr[i] += arr[i - 1];
+    }
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &a, &b);
+        printf("%d\n", arr[b] - arr[a - 1]);
+    }
+}
