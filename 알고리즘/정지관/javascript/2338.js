f64450f437037338cb2c6ee6dad6736326e50a68
jikwan <jikwan0327@gmail.com>
2023-01-23 10:26:16 +0900

    백준 2338번

--- 알고리즘/정지관/javascript/2338.js
+++ 알고리즘/정지관/javascript/2338.js
@@ -1,2 +1,9 @@
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString();
+const fs = require("fs");
+
+const input = fs.readFileSync("/dev/stdin").toString().split("\n").map(BigInt);
+
+let answer = input[0] + input[1] + "\n";
+answer += input[0] - input[1] + "\n";
+answer += input[0] * input[1];
+
+console.log(answer);
