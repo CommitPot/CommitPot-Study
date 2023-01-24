f26384b1141525537118a68e302d51166d851795
김철우 <102791231+chulwoo8747@users.noreply.github.com>
2023-01-24 23:59:41 +0900

    Create 2442.c

---
+++ 알고리즘/김철우/2442.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+	int a = 0;
+	
+	scanf_s("%d", &a);
+
+	for (int i = 0; i <= a; i++) {
+		for (int j = a-i; j > 0; j--) {
+			printf(" ");
+		}
+		for (int k = 0; k < 2*i-1; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
