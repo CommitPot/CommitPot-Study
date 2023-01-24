ece2428b671175839e475bc3812d7278782ca91f
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-24 23:57:15 +0900

    백준 26766번

---
+++ 알고리즘/유찬홍/26766.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf(" @@@   @@@ \n"
+               "@   @ @   @\n"
+               "@    @    @\n"
+               "@         @\n"
+               " @       @ \n"
+               "  @     @  \n"
+               "   @   @   \n"
+               "    @ @    \n"
+               "     @     \n");
+    }
+}
