e2c4796123ce0436718f15385999105a8ba6c6ca
jungho1209 <101508190+jungho1209@users.noreply.github.com>
2023-01-25 10:29:27 +0900

    Create 5338.c

---
+++ 알고리즘/이정호/5338.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    printf("       _.-;;-._\n");
+    printf("'-..-'|   ||   |\n");
+    printf("'-..-'|_.-;;-._|\n");
+    printf("'-..-'|   ||   |\n");
+    printf("'-..-'|_.-''-._|");
+    return 0;
+}
