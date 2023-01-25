cfdc2db2f9a91e5bc93205fa11d79bcae8736f2a
맥북 유찬홍 <chanhongy6@naver.com>
2023-01-25 16:58:12 +0900

    백준 26091번

---
+++ 알고리즘/유찬홍/26091.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int comp(const void *a, const void *b) {
+    return *(int *) a - *(int *) b;
+}
+
+int main() {
+    int n, m, count = 0, arr[100000];
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    qsort(arr, n, 4, comp);
+    int j = 0, k = n - 1;
+    while (j < k) {
+        int sum = arr[j] + arr[k];
+        if (sum < m) j++;
+        else {
+            j++, k--;
+            count++;
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
