ba7a5a590b731dab0988836a010fd93c56a8a2aa
김철우 <102791231+chulwoo8747@users.noreply.github.com>
2023-01-31 18:14:46 +0900

    백준 5338번

---
+++ 알고리즘/김철우/5338.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+	printf("       _.-;;-._\n");
+	printf("'-..-'|   ||   |\n");
+	printf("'-..-'|_.-;;-._|\n");
+	printf("'-..-'|   ||   |\n");
+	printf("'-..-'|_.-''-._|");
+}
