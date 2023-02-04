b4cfaf233f0008ab47d653915927124d28fe8ecd
김철우 <102791231+chulwoo8747@users.noreply.github.com>
2023-02-04 23:59:34 +0900

    Create 1546.c

---
+++ 알고리즘/김철우/1546.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main(void){
+	int i, max=0, count;
+	
+	scanf("%d", &count);
+	double study[count], avg=0;
+	
+	for(i=0;i<count;i++){
+		scanf("%lf", &study[i]);
+		if(study[i]>max)
+			max=study[i];
+	}
+	
+	for(i=0;i<count;i++){
+		study[i]=study[i]/max*100;
+		avg+=study[i];
+	}
+	avg/=count;
+	printf("%lf",avg);
+	
+}
