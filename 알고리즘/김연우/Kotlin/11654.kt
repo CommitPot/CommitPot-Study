e83c11f542e2068e23a81da2ac838577bea915ae
김연우 <102429539+yeon0821@users.noreply.github.com>
2023-02-11 23:12:41 +0900

    11654.kt

---
+++ 알고리즘/김연우/Kotlin/11654.kt
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+fun main() {
+    var br = BufferedReader(InputStreamReader(System.`in`));
+    var ch = StringTokenizer(br.readLine()).nextToken()[0]
+    print("${ch.toInt()}")
+}
