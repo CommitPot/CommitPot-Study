db774e9d54fd6ad9cda476ab42e7559834d1f105
김연우 <102429539+yeon0821@users.noreply.github.com>
2023-02-12 23:00:02 +0900

    2475.kt

---
+++ 알고리즘/김연우/Kotlin/2475.kt
@@ -0,0 +1,12 @@
+import java.util.*
+fun main() = with(Scanner(System.`in`)) {
+
+    val a = nextInt()
+    val b = nextInt()
+    val c = nextInt()
+    val d = nextInt()
+    val e = nextInt()
+
+    println((a*a+b*b+c*c+d*d+e*e)%10)
+
+}
