fe887b1426271203bdd9cf7408d806debe06cb08
김연우 <102429539+yeon0821@users.noreply.github.com>
2023-02-26 23:58:55 +0900

    2338.kt

---
+++ 알고리즘/김연우/Kotlin/2338.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextBigDecimal()
+    val b = sc.nextBigDecimal()
+
+    println(a + b)
+    println(a - b)
+    println(a * b)
+}
