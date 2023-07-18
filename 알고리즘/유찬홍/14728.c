5c71a3495405b38640c99ea31211100a634a0cfb
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2023-07-19 08:20:25 +0900

    백준 14728번

---
+++ 알고리즘/유찬홍/14728.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+int main() {
+    int n, k, w[1001], v[1001], dp[101][10001] = {0};
+    scanf("%d %d", &n, &k);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d %d", &w[i], &v[i]);
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= k; j++) {
+            if (j >= w[i]) {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    printf("%d", dp[n][k]);
+}
