c65735bb507247700f35dd30a46b5676a1e3e5b4
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-07-26 09:59:27 +0900

    백준 1181번 리팩토링
    시간 5160m/s -> 1296m/s

--- 알고리즘/정지관/javascript/1181.js
+++ 알고리즘/정지관/javascript/1181.js
@@ -1,22 +1,25 @@
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let size = parseInt(input[0]);
-let arr = [];
+let arr = []
 
-function isSame(element, i) {
-  if (element !== arr) {
-    return true;
-  }
+for(let i = 1; i <= Number(input[0]); i++) {
+    arr.push(input[[i]])
 }
 
-for (let i = 1; i <= size; i++) {
-  if (!arr.includes(input[i])) arr.push(input[i]);
-}
+let data = [...new Set(arr)]
 
-arr.sort();
-arr.sort(function (a, b) {
-  return a.length - b.length;
-});
-
-console.log(arr.join("\n"));
+data.sort((a,b) => {
+    if(a.length == b.length) {
+        if(a > b) {
+            return 1
+        } else {
+            return -1
+        }
+    }
+    return a.length - b.length
+    
+})
+data.map((res) => {
+    console.log(res)
+})
