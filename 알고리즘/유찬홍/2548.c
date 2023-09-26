b47cecb923af3dbae9b5418c4c65e87f485995f8
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2023-09-26 20:31:20 +0900

    백준 2548번

---
+++ 알고리즘/유찬홍/2548.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int comp(const void *a, const void *b) {
+    return *(int *) a - *(int *) b;
+}
+
+int main() {
+    int n, arr[20001];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    qsort(arr, n, 4, comp);
+    printf("%d", n % 2 == 0 ? arr[n / 2 - 1] : arr[n / 2]);
+}
