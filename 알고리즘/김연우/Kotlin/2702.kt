019cb73443f144238c62e01936895eb369f31992
김연우 <102429539+yeon0821@users.noreply.github.com>
2024-01-09 15:42:47 +0900

    2702.kt

--- 알고리즘/김연우/Kotlin/2702.kt
+++ 알고리즘/김연우/Kotlin/2702.kt
@@ -4,11 +4,15 @@ fun main() {
     repeat(t) {
         val (a, b) = readLine()!!.split(" ").map { it.toInt() }
         val lcm = findLCM(a, b)
-        val gcdValue = a.toBigInteger().gcd(b.toBigInteger()).toInt()
+        val gcdValue = gcd(a, b)
         println("$lcm $gcdValue")
     }
 }
 
+fun gcd(a: Int, b: Int): Int {
+    return if (b == 0) a else gcd(b, a % b)
+}
+
 fun findLCM(a: Int, b: Int): Int {
-    return (a * b) / a.toBigInteger().gcd(b.toBigInteger()).toInt()
+    return (a / gcd(a, b)) * b
 }
