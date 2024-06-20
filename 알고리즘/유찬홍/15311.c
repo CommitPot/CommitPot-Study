af8254b5c8cca6d03ee16fecc9b4c1bcbb9dbea8
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2024-06-20 11:00:02 +0900

    백준 15311번

---
+++ 알고리즘/유찬홍/15311.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("2000\n");
+    for (int i = 0; i < 1000; i++) printf("1 ");
+    for (int i = 0; i < 1000; i++) printf("1000 ");
+}
