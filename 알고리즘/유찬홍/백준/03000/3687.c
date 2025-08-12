f747e0ea4c973c8c010f59b3fd43f0877befc3de
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2025-08-12 20:37:33 +0900

    백준 3687번

--- 알고리즘/유찬홍/백준/03000/3687.c
+++ 알고리즘/유찬홍/백준/03000/3687.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
-
-long long f(long long x) {
-    long long r = 1;
-    while (x) x /= 10, r *= 10;
-    return r;
-}
+#define ll long long
 
 int main() {
     int n, t;
@@ -16,13 +11,12 @@ int main() {
         int k = 1;
         for (int i = n & 1 ? n - 3 : n - 2; i; i -= 2, k++) s[k] = '1';
         s[k] = 0;
-        long long dp[101] = {9, 9, 1, 7, 4, 2, 6, 8};
+        ll dp[101] = {9, 9, 1, 7, 4, 2, 6, 8};
         for (int i = 8; i <= n; i++) {
             dp[i] = 1e18;
             for (int j = 2; j < 8; j++) {
-                dp[i] = dp[i] < dp[i - j] * 10 + (dp[j] == 6 ? 0 : dp[j])
-                            ? dp[i]
-                            : dp[i - j] * 10 + (dp[j] == 6 ? 0 : dp[j]);
+                ll r = dp[i - j] * 10 + (dp[j] == 6 ? 0 : dp[j]);
+                dp[i] = dp[i] < r ? dp[i] : r;
             }
         }
         printf("%lld %s\n", dp[n], s);
