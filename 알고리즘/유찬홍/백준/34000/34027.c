10df0468e0c269c116a0f03ff75d9b78404a3d86
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2025-08-25 22:44:58 +0900

    백준 34027번

---
+++ 알고리즘/유찬홍/백준/34000/34027.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    int t, n;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &n);
+        printf("%d\n", sqrt(n) == (int) sqrt(n));
+    }
+}
