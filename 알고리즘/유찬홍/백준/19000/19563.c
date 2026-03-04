593b3068d37d1e3343f9f52f694101d5308911ad
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2026-03-04 12:44:57 +0900

    브론즈 1

---
+++ 알고리즘/유찬홍/백준/19000/19563.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    a = abs(a) + abs(b);
+    printf("%s", (a & 1) == (c & 1) && a <= c ? "YES" : "NO");
+}
