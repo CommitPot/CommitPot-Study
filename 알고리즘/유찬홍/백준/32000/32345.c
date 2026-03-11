8c5330276c1a64db33c4c37120dde9717f91ae5f
유찬홍 <102807101+Yoochanhong@users.noreply.github.com>
2026-03-11 12:52:22 +0900

    실버 3

---
+++ 알고리즘/유찬홍/백준/32000/32345.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int f(int v) {
+    return v == 'a' || v == 'e' || v == 'i' || v == 'o' || v == 'u';
+}
+
+int main() {
+    int t;
+    char s[300001];
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%s", s);
+        long long r = 1, j = -1;
+        for (int i = 0; s[i]; i++) {
+            if (f(s[i])) {
+                if (j < 0) j = i;
+                else r = r * (i - j) % 1000000007, j = i;
+            }
+        }
+        printf("%lld\n", j < 0 ? -1 : r > 1 ? r : 1);
+    }
+}
